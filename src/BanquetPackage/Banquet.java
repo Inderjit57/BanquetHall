66ec0f9bb25fa1fb086b9229fb9120d332cd946e
Inderjit57 <singhinderjit57@yahoo.com>
2022-02-04 16:40:58 -0500

    Updated calculate cess Method. Added condition for negative scenerio

--- src/BanquetPackage/Banquet.java
+++ src/BanquetPackage/Banquet.java
@@ -7,14 +7,10 @@ public class Banquet {
 	double beverageCost;
 	double tip;
 	double totalBaseCost;
-
 	double taxPercent;
 	double taxOnBaseCost;
-
 	double cessPercent;
 	double noOfGuests;
-	boolean isValid; // No. of guests
-
 	double totalCost;
 
 	public Banquet() {
@@ -38,27 +34,20 @@ public class Banquet {
 
 	double calculateCess(double noOfGuests) {
 		this.noOfGuests = noOfGuests;
+	
 		if (noOfGuests <= 40) {
 			cessPercent = 0.04 * totalBaseCost; // CESS IS 4% OF TOTAL BILL BEFORE TAX
-			if (noOfGuests < 0) {
-				System.out.println("INVALID ENTRY"); // ANY NEGATIVE NUMBER IS NOT APPLICABLE
-				isValid = false;
-			}
 		} else if (noOfGuests > 40 && noOfGuests <= 80) {
 			cessPercent = 0.08 * totalBaseCost; // CESS IS 8% OF TOTAL BILL BEFORE TAX
 		} else if (noOfGuests > 80 && noOfGuests <= 150) {
 			cessPercent = 0.1 * totalBaseCost; // CESS IS 10% OF TOTAL BILL BEFORE TAX
 		} else if (noOfGuests > 150) {
 			cessPercent = 0.125 * totalBaseCost; // CESS IS 12.5% OF TOTAL BILL BEFORE TAX
-			if (noOfGuests >= 500) {
-				System.out.println("MAX CAPACITY IS 500"); // Banquet Hall Max Capacity is 500
-				isValid = false;
-			}
 		}
 		return cessPercent;
 	}
 
-	double calculateTheTotalCost() {
+	double calculateTotalCost() {
 		totalCost = (totalBaseCost + taxOnBaseCost + cessPercent);
 		return totalCost;
 	}
